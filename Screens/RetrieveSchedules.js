a6b4101883f360cc8001db8a604e7783464babb9
Nokuthaba <710178@students.wits.ac.za>
2022-05-23 01:03:21 +0200

    Function to Retrieve Schedule data

--- Screens/RetrieveSchedules.js
+++ Screens/RetrieveSchedules.js
@@ -1,35 +1,34 @@
 import {firebase} from "../firebase"
-
+import {authUserID} from './Login'
 import { v4 as uuidv4 } from 'uuid';
 
 
-
+const user_id = authUserID;
 
 
 export async function getSchedule(onReceiveList) {
 
-    var schedules = []
-    
-    
-    firebase.database().ref('/schedules').on('value',snapshot=>{
-        //if(snapshot.val()==email)
-       
-
-    
-           const key = snapshot.forEach(function(data) {
-            
-            schedules.push({key1:schedules.length+1,created_at:snapshot.child(data.key+"/created_at").val(),
-            hospital_ID:snapshot.child(data.key+"/hospital_id").val(),
-            ID:snapshot.child(data.key+"/id").val(),
-            specialty_duration:snapshot.child(data.key+"/specialty_duration").val(),
-            specialty_id:snapshot.child(data.key+"/specialty_id").val(),
-            student_id:snapshot.child(data.key+"/student_id").val(),
-            updated_at:snapshot.child(data.key+"/updated_at").val(),
-            week_no:snapshot.child(data.key+"/week_no").val(),
-            })
-        });
-
-    })
-
+    var schedules = [] // Create empty array to store all items in database
+    const docRef = firebase.firestore().collection("schedules");
+    var snapshot = await docRef.where("student_id", "==", user_id).get();
+    if (snapshot.empty) {
+        console.log('No matching documents.');
+        return;
+      }  
+      
+      snapshot.forEach(function(doc) {
+        schedules.push({key1: schedules.length + 1,
+        created_at:doc.data().created_at,
+        hospital_ID:doc.data().hospital_id,
+        ID:doc.data().id,
+        specialty_duration:doc.data().specialty_duration,
+        specialty_id:doc.data().specialty_id,
+        student_id:doc.data().student_id,
+        updated_at:doc.data().updated_at,
+        start_date:doc.data().start_date,
+        end_date: doc.data().end_date,
+        SpecialtyName:doc.data().SpecialtyName
+        })
+      })
    onReceiveList(schedules);
   }
